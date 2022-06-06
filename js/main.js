5cd809ff65f521654d88344cfe8c335e8d9cd431
RobinsonElena <sherbakovaen2011@yandex.ru>
2022-06-06 13:41:35 -0400

    Вносит исправления во вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomIntInclusive(min, max) {
-  if (min >= 0 && max > min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  } else {
-    return 'Диапазон может быть только положительный, включая ноль. Введите корректный диапазон чисел';
+const getRandomIntInclusive = (min, max) => {
+  if (min < 0 || max < min) {
+    throw new RangeError ('Диапазон может быть только положительный, включая ноль. Введите корректный диапазон чисел');
   }
-}
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+};
 
 getRandomIntInclusive(0, 10);
 
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/number/tofixed
 
-function getRandomInclusive(min, max, digits) {
-  if (min >= 0 && max > min) {
-    return (Math.floor(Math.random() * (max - min + 1)) + min).toFixed(digits); //Максимум и минимум включаются
-  } else {
-    return 'Диапазон может быть только положительный, включая ноль. Введите корректный диапазон чисел';
+const getRandomInclusive = (min, max, digits=5) => {
+  if (min < 0 || max < min) {
+    throw new RangeError ('Диапазон может быть только положительный, включая ноль. Введите корректный диапазон чисел');
   }
-}
+  return +(Math.floor(Math.random() * (max - min + 1)) + min).toFixed(digits); //Максимум и минимум включаются
+};
 
 getRandomInclusive(0, 10, 5);
