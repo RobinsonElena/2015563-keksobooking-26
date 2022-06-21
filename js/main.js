62d2cddc0c2545d3f15668a1f000e5d7351c5447
RobinsonElena <sherbakovaen2011@yandex.ru>
2022-06-21 15:11:45 -0400

    Дорабатывает функции для создания массива объявлений

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-/*const getRandomIntInclusive = (min, max) => {
-  if (min < 0 || max <= min) {
-    throw new RangeError ('Диапазон может быть только положительный, включая ноль. Введите корректный диапазон чисел');
-  }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-};
-
-getRandomIntInclusive(0, 10);
-
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/number/tofixed
-
-const getRandomInclusive = (min, max, digits = 5) => {
-  if (min < 0 || max <= min) {
-    throw new RangeError ('Диапазон может быть только положительный, включая ноль. Введите корректный диапазон чисел');
-  }
-  return +(Math.random() * (max - min + 1) + min).toFixed(digits); //Максимум и минимум включаются
-};
-
-getRandomInclusive(0, 10);*/
-
-//Создать массив из 10 сгенерированных JS-объектов
-
 const TITLES = [
   'В центре',
   'Бизнес-центр',
@@ -32,6 +5,7 @@ const TITLES = [
   'На окраине',
   'Подходит всем',
 ];
+
 const TYPES = [
   'palace',
   'flat',
@@ -39,11 +13,13 @@ const TYPES = [
   'bungalow',
   'hotel',
 ];
+
 const TIMES = [
   '12:00',
   '13:00',
   '14:00',
 ];
+
 const FEATURES = [
   'wifi',
   'dishwasher',
@@ -52,6 +28,7 @@ const FEATURES = [
   'elevator',
   'conditioner',
 ];
+
 const DESCRIPTION = [
   'Евродизайн',
   'В старых традициях',
@@ -59,6 +36,7 @@ const DESCRIPTION = [
   'Дружелюбный к животным',
   'В дали от цивилизации',
 ];
+
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
@@ -72,7 +50,7 @@ const LNG_MAX = 139.80000;
 
 const RENT_OFFER_COUNT = 10;
 
-const getRandomPositiveInteger = (a, b) => {
+const getRandomPositiveInteger = (a, b) => {   //Функция генерации положительного числа
   if (a < 0 || b <= a) {
     throw new RangeError ('Диапазон может быть только положительный, включая ноль. Введите корректный диапазон чисел');
   }
@@ -82,28 +60,44 @@ const getRandomPositiveInteger = (a, b) => {
   return Math.floor(result);
 };
 
-const getRandomInclusive = (min, max, digits = 5) => {
+const getRandomFloat = (min, max, digits = 5) => {   //Функция генерации положительного числа с плавающей точкой
   if (min < 0 || max <= min) {
     throw new RangeError ('Диапазон может быть только положительный, включая ноль. Введите корректный диапазон чисел');
   }
-  return +(Math.random() * (max - min + 1) + min).toFixed(digits); //Максимум и минимум включаются
+  return +(Math.random() * (max - min) + min).toFixed(digits); //Максимум и минимум включаются
 };
 
-const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];   //Функция генерации элемента из массива
 
-const createOffer = (index) => {
-  location = {
-    lat: getRandomInclusive(LAT_MIN, LAT_MAX, 5),
-    lng: getRandomInclusive(LNG_MIN, LNG_MAX, 5)
+const getShuffledArray = (elements) => {   //Функция перемешивания и генерации нового массива
+  const newArray = elements.slice();
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+};
+
+let avatarNumber = 0;
+const getAvatarNumber = () => {   //Функция для получения неповторяющегося адреса изображения с "0" для однозначных
+  for (let i = 1; i <= RENT_OFFER_COUNT; i++){
+    avatarNumber += 1;
+    const formattedAvatarNumber = (avatarNumber < 10) ? `0${avatarNumber}` : avatarNumber;
+    return `img/avatars/user${formattedAvatarNumber}.png`;
+  }
+};
+
+const createRentOffers = () => {   //Функция создания объявления
+  const location = {
+    lat: getRandomFloat(LAT_MIN, LAT_MAX),
+    lng: getRandomFloat(LNG_MIN, LNG_MAX)
   };
 
   return {
-    /*autor: {
-      if (index.lenght < 2) {
-        result = `img/avatars/user0${index + 1}.png`};
-      else {result = `img/avatars/user${index + 1}.png`}; //avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
-      avatar
-    },*/
+    author: {
+      avatar: getAvatarNumber()
+    },
+
     offer: {
       title: getRandomArrayElement(TITLES),
       address: `${location.lat}, ${location.lng}`,
@@ -113,17 +107,15 @@ const createOffer = (index) => {
       guests: getRandomPositiveInteger(0, 3),
       checkin: getRandomArrayElement(TIMES),
       checkout: getRandomArrayElement(TIMES),
-      features: getRandomArrayElement(FEATURES),  //Значения не должны повторяться. Как это сделать?
+      features: getShuffledArray(FEATURES),
       description: getRandomArrayElement(DESCRIPTION),
-      photos: getRandomArrayElement(PHOTOS),   //массив строк — массив случайной длины из значений. Переделать
+      photos: getShuffledArray(PHOTOS),
     },
+
+    location
   };
 };
 
-const rentOffer = () => Array.from({length: RENT_OFFER_COUNT}, createOffer);
+const rentOffers = () => Array.from({length: RENT_OFFER_COUNT}, createRentOffers);   //Функция создания массива из объявлений
 
-getRandomInclusive,
-getRandomArrayElement,
-getRandomPositiveInteger,
-
-console.log(rentOffer);
+console.log(rentOffers());
